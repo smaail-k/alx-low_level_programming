425135f4525bdbcb0b338742123fe26c483e0b74
smaail-k <liamski.45@gmail.com>
2023-03-26 02:13:31 +0000

    fizz

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,12 +8,12 @@
 
 void print_most_numbers(void)
 {
-		char c;
+	char c;
 
-			for (c = '0'; c <= '9'; c++)
+for (c = '0'; c <= '9'; c++)
 					{
-								if (!(c == '2' || c == '4'))
-												_putchar(c);
-									}
-				_putchar('\n');
+	if (!(c == '2' || c == '4'))
+	_putchar(c);
+					}
+	_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,15 +6,14 @@
      */
 void print_line(int n)
 {
-		if (n <= 0)
+	if (n <= 0)
 				{
 							_putchar('\n');
-								} else
-										{
-													int i;
+								} 
+	else								
+		int i;													for (i = 1; i <= n; i++)
 
-															for (i = 1; i <= n; i++)
-																		{
+	{
 																						_putchar('_');
 																								}
 																	_putchar('\n');
