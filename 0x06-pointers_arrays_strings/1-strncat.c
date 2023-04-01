d00b9461737dc0b4d101bc3535752a193207760b
smaail-k <liamski.45@gmail.com>
2023-04-01 21:19:28 +0000

    str to magic

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,30 +1,25 @@
 #include "main.h"
 /**
-   * _strncat - concatenates two strings,
-    * @dest: destination.
-     * @src: source.
-      * @n: amount of bytes used from src.
-       * Return: the pointer to dest.
-        */
+ * _strncat - concatenates two strings,
+ * @dest: destination.
+ * @src: source.
+ * @n: amount of bytes used from src.
+ * Return: the pointer to dest.
+ */
 char *_strncat(char *dest, char *src, int n)
 {
 		int i;
-			int j;
+		int j;
 
-				i = 0;
-					while (dest[i] != '\0')
-							{
-										i++;
-											}
+		i = 0;
+		while (dest[i] != '\0')
+		{
+			i++;
+		}
+		j = 0;
+		while (j < n && src[j] != '\0')
 
-						j = 0;
-							while (j < n && src[j] != '\0')
-									{
-												dest[i] = src[j];
-														i++;
-																j++;
-																	}
-								dest[i] = '\0';
-
-									return (dest);
-}
+		{
+		dest[i] = src '\0';
+		return (dest);
+	}
