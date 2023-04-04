c89cd73689e737bf2028aebaaed65d44abdb46a8
smaail-k <liamski.45@gmail.com>
2023-04-04 23:47:43 +0000

    correcct code

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,13 +10,14 @@ char *_strpbrk(char *s, char *accept)
 	int k;
 
 	while (*s)
-					{
-	for (k = 0; accept[k]; k++)
 			{
+	for (k = 0; accept[k]; k++)
+	{
 	if (*s == accept[k])
 	return (s);
-			}
+	}
 	s++;
-					}
+			}
+
 	return ('\0');
 }
